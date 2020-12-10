566503ff7790b3bea896793013c934e1f65f0aec
Davi Martins <davibarroso12399@gmail.com>
2020-12-10 15:08:43 -0300

    First Version

--- digitalclock.c
+++ digitalclock.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main()
 {
+  time_t currentTime;
+
+  while(0 == 0)
+  {
+      time(&currentTime);
+      printf("\n\n\t\t%s\n", ctime(&currentTime));
+      system("cls");
+  }
+
     return 0;
 }
